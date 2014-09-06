d0d9068d53ad01deac70ddc5076f9811ff906290
daemon2010 <alexander.karamushko@gmail.com>
2014-09-06 17:16:36 +0300

    fixes

--- application/views/gyik/index.php
+++ application/views/gyik/index.php
@@ -1,1 +1,87 @@
-FAQ
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('#faqList').simpleFAQ();
+  });
+</script>
+
+<nav>
+    <ul>
+        <?php echo View::factory('template/menu', get_defined_vars())->render(); ?>
+    </ul>
+</nav>
+<div class="clear"></div>
+			<section class="faq" class="rounded">
+				<h1>Gyakran Ismételt Kérdések</h1>
+				<ul id="faqList">
+					<li>
+					<p class="question">+ 1. Mi is az a Goodiebox pontosan?</p>
+					<div class="answer">
+					Answer 1
+					</div>
+					</li>
+
+					<li>
+					<p class="question">+ 2. Hogyan tudok rendelni a kutyusomnak?</p>
+					<div class="answer">
+					Answer 2
+					</div>
+					</li>
+
+					<li>
+					<p class="question">+ 3. Több kutyám is van. Tudok egyszerre több dobozt is rendelni?</p>
+					<div class="answer">
+					Answer 3
+					</div>
+					</li>
+
+					<li>
+					<p class="question">+ 4. Mennyibe kerül és hogyan fizethetek?</p>
+					<div class="answer">
+					Answer 4
+					</div>
+					</li>
+
+					<li>
+					<p class="question">+ 5. Ha megrendeltem, mikor kapom meg?</p>
+					<div class="answer">
+					Answer 5
+					</div>
+					</li>
+
+					<li>
+					<p class="question">+ 6. Hogyan kapom meg a dobozom?</p>
+					<div class="answer">
+					Answer 6
+					</div>
+					</li>
+
+					<li>
+					<p class="question">+ 7. Meg lehet tudni, hogy mi lesz a következő havi dobozban? Választhatok?</p>
+					<div class="answer">
+					Answer 7
+					</div>
+					</li>
+
+					<li>
+					<p class="question">+ 8. Van bármilyen rejtett költség?</p>
+					<div class="answer">
+					Answer 8
+					</div>
+					</li>
+
+					<li>
+					<p class="question">+ 9. Nem érkezett meg a Goodiebox, pedig regsztráltam, vagyis megrendeltem.</p>
+					<div class="answer">
+					Answer 9
+					</div>
+					</li>
+
+					<li>
+					<p class="question">+ 10. Van-e mód a visszaküldésre, ha elégedetlen vagyok?</p>
+					<div class="answer">
+					Answer 10
+					</div>
+					</li>
+				</ul>
+				 <img src="<?= URL::base(TRUE, FALSE) ?>/assets/img/faq-img.png" alt="FAQS">
+			</section>
--- application/views/kapcsolat/index.php
+++ application/views/kapcsolat/index.php
@@ -1,1 +1,51 @@
-Contact us
+<nav>
+    <ul>
+        <?php echo View::factory('template/menu', get_defined_vars())->render(); ?>
+    </ul>
+</nav>
+<div class="clear"></div>
+<section class="contact" class="rounded">
+    <h1>Írj nekünk!</h1>
+    <div class="contact-text">
+        <p>Mindig örülünk, ha üzenetet kapunk! </br>Van egy jó ötleted? Valami nem tetszett vagy nagyon is? </br>Vannak kérdéseid és a választ nem találod a GYIK menü pontban?</br>Ne rejtegesd a véleményed, oszd meg velünk és mi válaszolunk neked!</p>
+    </div>
+    <div class="contact-cont-left">
+        <div class="contact-address">
+            Kapcsolat információ:</br>
+            <span>GOODIEBOX </br>
+                Web Apps Consult Kft.</span></br>
+            Web Apps Consult Kft. </br>
+            <span>1053 Budapest </br>
+                Kossuth Lajos utca 7-9.</span> </br>
+            E-mail cím: </br>
+            <span>info@goodiebox.hu</span>
+        </div> <!--End cotact address-->
+
+        <div class="contact-us-form">
+            <form>
+                <label for="last-name">Vezetéknév*</label>
+                <input type="text" name="last-name" class="rounded" id="">
+
+                <label for="last-name">Keresztnév*</label>
+                <input type="text" name="last-name" class="rounded" id="">
+
+                <label for="last-name">E-mail cím*</label>
+                <input type="text" name="last-name" class="rounded" id="">
+
+                <label for="last-name">Tárgy</label>
+                <input type="text" name="last-name" class="rounded" id="">
+
+                <textarea placeholder="Kezdj egy gépelni..." class="rounded"></textarea>
+            </form>
+            *Kötelező mezők
+            <input type="submit" value="ELKÜLDÖM" class="rounded">
+        </div>
+    </div> <!--End contact-cont-left-->
+    <div class="contact-cont-right">
+        <ul>
+            <li><img src="<?= URL::base(TRUE, FALSE) ?>/assets/img/contact-img1.jpg"></li>
+            <li><img src="<?= URL::base(TRUE, FALSE) ?>/assets/img/contact-img2.jpg"></li>
+            <li><img src="<?= URL::base(TRUE, FALSE) ?>/assets/img/contact-img3.jpg"></li>
+        </ul>
+    </div> <!--End contact-cont-right-->
+</section>
