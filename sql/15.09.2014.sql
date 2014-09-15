8d5157717095dbed45410b83aad862d39f5ee9dc
Karam <alexander.karamushko@gmail.com>
2014-09-15 18:32:13 +0300

    fixes

---
+++ sql/15.09.2014.sql
@@ -0,0 +1,170 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.0-beta2
+-- http://www.phpmyadmin.net
+--
+-- Хост: localhost
+-- Время создания: Сен 15 2014 г., 18:30
+-- Версия сервера: 5.5.37-0ubuntu0.13.10.1
+-- Версия PHP: 5.5.3-1ubuntu2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `goodiebox`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `ordered_box` int(11) NOT NULL,
+  `comment` text COLLATE utf8_swedish_ci NOT NULL,
+  `customer_name` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `customer_street_address` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `customer_city` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `customer_postcode` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `customer_telephone` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `customer_email_address` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `delivery_name` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `delivery_street_address` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `delivery_city` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `delivery_postcode` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `last_modified` datetime DEFAULT NULL,
+  `date_purchased` datetime DEFAULT NULL,
+  `orders_status` int(5) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `roles`
+--
+
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) CHARACTER SET utf8 NOT NULL,
+  `description` varchar(255) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=3 ;
+
+--
+-- Дамп данных таблицы `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `description`) VALUES
+(1, 'login', 'Login privileges, granted after account confirmation'),
+(2, 'admin', 'Administrative user, has access to everything.');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `roles_users`
+--
+
+CREATE TABLE IF NOT EXISTS `roles_users` (
+  `user_id` int(10) unsigned NOT NULL,
+  `role_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `fk_role_id` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+--
+-- Дамп данных таблицы `roles_users`
+--
+
+INSERT INTO `roles_users` (`user_id`, `role_id`) VALUES
+(1, 1),
+(1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(127) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `email` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `customer_firstname` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `customer_lastname` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `customer_telephone` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `customer_zip` varchar(15) COLLATE utf8_swedish_ci NOT NULL,
+  `customer_city` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `logins` int(11) NOT NULL,
+  `last_login` varchar(50) COLLATE utf8_swedish_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_email` (`username`),
+  KEY `users_index` (`id`),
+  KEY `fk_users` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=3 ;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `customer_firstname`, `customer_lastname`, `customer_telephone`, `customer_zip`, `customer_city`, `logins`, `last_login`) VALUES
+(1, 'karam@karam.org.ua', '21232f297a57a5a743894a0e4a801fc3', 'karam@karam.org.ua', '', '', '', '', '', 7, '1410773856'),
+(2, 'alex@onlamp.info', '8dbb643c53ed9567bbb1e1f73fe8b8b193a0472f109c0feb00fdb2e8681debb5', 'alex@onlamp.info', '', '', '', '', '', 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user_tokens`
+--
+
+CREATE TABLE IF NOT EXISTS `user_tokens` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `user_agent` varchar(40) CHARACTER SET utf8 NOT NULL,
+  `token` varchar(40) CHARACTER SET utf8 NOT NULL,
+  `created` int(10) unsigned NOT NULL,
+  `expires` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_token` (`token`),
+  KEY `fk_user_id` (`user_id`),
+  KEY `expires` (`expires`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=1 ;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `roles_users`
+--
+ALTER TABLE `roles_users`
+  ADD CONSTRAINT `roles_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `roles_users_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `user_tokens`
+--
+ALTER TABLE `user_tokens`
+  ADD CONSTRAINT `user_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
