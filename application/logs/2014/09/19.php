1b1a947f439efff9223a557e4e2ffd269a54a1d4
daemon2010 <alexander.karamushko@gmail.com>
2014-09-19 23:19:27 +0300

    fixes

--- application/logs/2014/09/19.php
+++ application/logs/2014/09/19.php
@@ -190,3 +190,14 @@
 #8 /var/www/goodiebox.priv/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
 #9 /var/www/goodiebox.priv/index.php(118): Kohana_Request->execute()
 #10 {main} in /var/www/goodiebox.priv/modules/database/classes/Kohana/Database/Query.php:251
+2014-09-19 23:18:51 --- EMERGENCY: ORM_Validation_Exception [ 0 ]: Failed to validate array ~ MODPATH/orm/classes/Kohana/ORM.php [ 1275 ] in /var/www/goodiebox.priv/modules/orm/classes/Kohana/ORM.php:1302
+2014-09-19 23:18:51 --- DEBUG: #0 /var/www/goodiebox.priv/modules/orm/classes/Kohana/ORM.php(1302): Kohana_ORM->check(NULL)
+#1 /var/www/goodiebox.priv/modules/orm/classes/Kohana/ORM.php(1421): Kohana_ORM->create(NULL)
+#2 /var/www/goodiebox.priv/application/classes/Controller/Order.php(81): Kohana_ORM->save()
+#3 /var/www/goodiebox.priv/system/classes/Kohana/Controller.php(84): Controller_Order->action_step3()
+#4 [internal function]: Kohana_Controller->execute()
+#5 /var/www/goodiebox.priv/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Order))
+#6 /var/www/goodiebox.priv/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#7 /var/www/goodiebox.priv/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#8 /var/www/goodiebox.priv/index.php(118): Kohana_Request->execute()
+#9 {main} in /var/www/goodiebox.priv/modules/orm/classes/Kohana/ORM.php:1302
