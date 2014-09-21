0bddbb6fbe6b23a19fda6b2fb9c531576b88154c
daemon2010 <alexander.karamushko@gmail.com>
2014-09-21 16:09:29 +0300

    fixes

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -3,14 +3,14 @@
 <div id="footer-wrapper" class="rounded">
     <footer>
         <ul>
-            <li id="post"><a href="#">1053 Budapest </br>Kossuth L. u. 7-9.</a></li>
-            <li><a href="#">             </a></li>
+            <li id="post"><a href="#">1053 Budapest Kossuth L. u. 7-9.</a></li>
+            <!-- li id="telephone"><a href="#">+36 30 500 6785</a></li -->
             <li id="email"><a href="mailto:info@goodiebox.hu">info@goodiebox.hu</a></li>
-            <li id="rss"><a href="#">RSS</a></li>
+            <!-- li id="rss"><a href="/?feed=rss2">RSS</a></li -->
             <li id="fb"><a href="http://facebook.com/goodieboxhu">facebook.com/goodieboxhu</a></li>
-            <li id="twt"><a href="http://twitter.com/godieboxhu">twitter.com/godieboxhu</a></li>
             <li id="aszf"><a href="/tos">ÁSZF</a></li>
             <li id="adat"><a href="/privacy">Adatvédelem</a></li>
+            <li id="twt"><a href="http://twitter.com/goodieboxhu">twitter.com/goodieboxhu</a></li>
         </ul>
         <div class="clear"></div>
         <div class="copy-write">
