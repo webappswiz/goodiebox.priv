605dffc21e74833550c83d0fc9c01b69987d509b
Karam <alexander.karamushko@gmail.com>
2014-09-22 11:46:23 +0300

    fixes

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -62,6 +62,25 @@
             FB.ui(obj, callback);
         }
     </script>
+    <script>
+    < script >
+                (function(i, s, o, g, r, a, m) {
+                    i['GoogleAnalyticsObject'] = r;
+                    i[r] = i[r] || function() {
+                        (i[r].q = i[r].q || []).push(arguments)
+                    }, i[r].l = 1 * new Date();
+                    a = s.createElement(o),
+                            m = s.getElementsByTagName(o)[0];
+                    a.async = 1;
+                    a.src = g;
+                    m.parentNode.insertBefore(a, m)
+                })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-53180118-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+</script>
 </head>
 <body>
     <div class="container">
