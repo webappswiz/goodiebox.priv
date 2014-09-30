d9e4f494ba6cc834636ba0371bcfe7eb3fae6f06
Alex <alexander.karamushko@gmail.com>
2014-09-30 17:00:44 +0300

    fixes

--- application/classes/Controller/Order.php
+++ application/classes/Controller/Order.php
@@ -38,7 +38,8 @@ class Controller_Order extends Controller_Core {
             } else{
                 $s = '';
             }
-        }
+        } else
+            $s=0;
 
         $invoice = '
             <!DOCTYPE html>
