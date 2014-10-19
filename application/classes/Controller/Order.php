a8a7dae565b8b5dc5a757826687772bbbb97346a
Alex Karamushko <alexander.karamushko@gmail.com>
2014-10-19 16:10:24 +0300

    fixes

--- application/classes/Controller/Order.php
+++ application/classes/Controller/Order.php
@@ -138,13 +138,13 @@ and open the template in the editor.
                 <td style="border-left: 2px solid;padding: 0px;margin: 0px;width:25%;height: 30px;"></td>
                 <td style="margin: 0px;width:25%;height: 30px;"></td>
                 <td style="border-top: 2px solid;padding: 0px;padding-top: 5px;padding-bottom: 5px;margin: 0px;width:25%;height: 30px;font-size: 10px;">Kedvezmény:<br/><br/>Házhozszállítás:</td>
-                <td style="border-top: 2px solid;padding: 0px;padding-top: 5px;padding-bottom: 5px;margin: 0px;width:13%;height: 30px;font-size: 10px;border-right: 2px solid;text-align:right;">' . number_format((float)$discount, 2, '.', '') . '&nbsp&nbsp<br/><br/>0,00&nbsp&nbsp</td>
+                <td style="border-top: 2px solid;padding: 0px;padding-top: 5px;padding-bottom: 5px;margin: 0px;width:13%;height: 30px;font-size: 10px;border-right: 2px solid;text-align:right;">' . number_format((float)$discount, 2, '.', '') . '&nbsp;&nbsp;<br/><br/>0,00&nbsp&nbsp</td>
             </tr>
             <tr style="padding: 0px">
                 <td style="border-left: 2px solid;padding: 0px;margin: 0px;width:25%;height: 30px;"></td>
                 <td style="margin: 0px;width:25%;height: 30px;"></td>
                 <td style="border-top: 2px solid;padding: 0px;margin: 0px;width:25%;height: 30px;font-size: 14px;">Összesen:</td>
-                <td style="border-top: 2px solid;padding: 0px;padding-top: 5px;margin: 0px;width:13%;height: 30px;font-size: 14px;border-right: 2px solid;text-align:right;">' . number_format((float)$total_price, 2, '.', '') . '&nbsp&nbsp</td>
+                <td style="border-top: 2px solid;padding: 0px;padding-top: 5px;margin: 0px;width:13%;height: 30px;font-size: 14px;border-right: 2px solid;text-align:right;">' . number_format((float)$total_price, 2, '.', '') . '&nbsp;&nbsp;</td>
             </tr>
             <tr style="padding: 0px">
                 <td style="vertical-align: bottom;padding: 0px;margin: 0px;width:50%;height: 65px;border-left: 2px solid;font-size: 10px;border-bottom: 2px solid;border-right: 2px solid;" colspan="4">Az ÁFA kulcs: AAM (alanyi mentes)</td>
@@ -729,11 +729,10 @@ and open the template in the editor.
                         $output = $pdf->output();
                         file_put_contents(DOCROOT . 'orders/gift_' . $ord->id . '.pdf', $output);
                         $template = ORM::factory('Templates', 3);
+                        $body = str_replace('[firstname]', $step1['firstname'], $template->template_text);
                         if (isset($step1['delay'])) {
-                            $body = str_replace('[firstname]', $this->current_user->customer_firstname, $template->template_text);
                             $to = $this->current_user->email;
                         } else {
-                            $body = str_replace('[firstname]', $step1['firstname'], $template->template_text);
                             $to = $step1['friend_email'];
                         }
                     }
