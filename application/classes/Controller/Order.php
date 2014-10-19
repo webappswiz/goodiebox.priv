81ce157e5fbbf828a946fe3de1e880bbf7cd6eef
Alex Karamushko <alexander.karamushko@gmail.com>
2014-10-19 15:20:48 +0300

    fixes

--- application/classes/Controller/Order.php
+++ application/classes/Controller/Order.php
@@ -762,22 +762,22 @@ and open the template in the editor.
         <style>
             @font-face {
                 font-family: \'gillsansmt\';
-                src: url(\'<?=URL::base(TRUE, FALSE)?>assets/fonts/gillsansmt.eot\');
-                src: local(\'<?=URL::base(TRUE, FALSE)?>assets/fonts/gillsansmt\'), url(\'<?=URL::base(TRUE, FALSE)?>assets/fonts/gillsansmt.woff\') format(\'woff\'), url(\'<?=URL::base(TRUE, FALSE)?>assets/fonts/gillsansmt.ttf\') format(\'truetype\');}
+                src: url(\''.URL::base(TRUE, FALSE).'assets/fonts/gillsansmt.eot\');
+                src: local(\''.URL::base(TRUE, FALSE).'assets/fonts/gillsansmt\'), url(\''.URL::base(TRUE, FALSE).'assets/fonts/gillsansmt.woff\') format(\'woff\'), url(\''.URL::base(TRUE, FALSE).'assets/fonts/gillsansmt.ttf\') format(\'truetype\');}
             @font-face {
                 font-family:\'GilgongoKaps-Regular\';
-                src: url(\'<?=URL::base(TRUE, FALSE)?>assets/fonts/GilgongoKaps_latin+latin-ext_gdi.eot\');
-                src: url(\'<?=URL::base(TRUE, FALSE)?>assets/fonts/GilgongoKaps_latin+latin-ext_gdi.eot?#iefix\') format(\'embedded-opentype\'),
-                    url(\'<?=URL::base(TRUE, FALSE)?>assets/fonts/GilgongoKaps_latin+latin-ext_gdi.woff\') format(\'woff\'),
-                    url(\'<?=URL::base(TRUE, FALSE)?>assets/fonts/GilgongoKaps_latin+latin-ext_gdi.ttf\') format(\'truetype\'),
-                    url(\'<?=URL::base(TRUE, FALSE)?>assets/fonts/GilgongoKaps_latin+latin-ext_gdi.svg#GilgongoKaps-Regular\') format(\'svg\');}
+                src: url(\''.URL::base(TRUE, FALSE).'assets/fonts/GilgongoKaps_latin+latin-ext_gdi.eot\');
+                src: url(\''.URL::base(TRUE, FALSE).'assets/fonts/GilgongoKaps_latin+latin-ext_gdi.eot?#iefix\') format(\'embedded-opentype\'),
+                    url(\''.URL::base(TRUE, FALSE).'assets/fonts/GilgongoKaps_latin+latin-ext_gdi.woff\') format(\'woff\'),
+                    url(\''.URL::base(TRUE, FALSE).'assets/fonts/GilgongoKaps_latin+latin-ext_gdi.ttf\') format(\'truetype\'),
+                    url(\''.URL::base(TRUE, FALSE).'assets/fonts/GilgongoKaps_latin+latin-ext_gdi.svg#GilgongoKaps-Regular\') format(\'svg\');}
             @font-face {
                 font-family:\'GilgongoMutombo-Regular\';
-                src: url(\'<?=URL::base(TRUE, FALSE)?>assets/fonts/GilgongoMutombo_latin+latin-ext_gdi.eot\');
-                src: url(\'<?=URL::base(TRUE, FALSE)?>assets/fonts/GilgongoMutombo_latin+latin-ext_gdi.eot?#iefix\') format(\'embedded-opentype\'),
-                    url(\'<?=URL::base(TRUE, FALSE)?>assets/fonts/GilgongoMutombo_latin+latin-ext_gdi.woff\') format(\'woff\'),
-                    url(\'<?=URL::base(TRUE, FALSE)?>assets/fonts/GilgongoMutombo_latin+latin-ext_gdi.ttf\') format(\'truetype\'),
-                    url(\'<?=URL::base(TRUE, FALSE)?>assets/fonts/GilgongoMutombo_latin+latin-ext_gdi.svg#GilgongoMutombo-Regular\') format(\'svg\');}
+                src: url(\''.URL::base(TRUE, FALSE).'assets/fonts/GilgongoMutombo_latin+latin-ext_gdi.eot\');
+                src: url(\''.URL::base(TRUE, FALSE).'assets/fonts/GilgongoMutombo_latin+latin-ext_gdi.eot?#iefix\') format(\'embedded-opentype\'),
+                    url(\''.URL::base(TRUE, FALSE).'assets/fonts/GilgongoMutombo_latin+latin-ext_gdi.woff\') format(\'woff\'),
+                    url(\''.URL::base(TRUE, FALSE).'assets/fonts/GilgongoMutombo_latin+latin-ext_gdi.ttf\') format(\'truetype\'),
+                    url(\''.URL::base(TRUE, FALSE).'assets/fonts/GilgongoMutombo_latin+latin-ext_gdi.svg#GilgongoMutombo-Regular\') format(\'svg\');}
             .box{width:290px; height:145px; float:left;font-family: \'gillsansmt\';letter-spacing:2px;}
             @media only screen and (max-width:480px){ img{width: 100%; height: auto;}.box-row td{padding: 10px !important;}h2{font-size: 24px!important;} h2 span{font-size: 16px!important;}}
         </style>	
@@ -785,14 +785,14 @@ and open the template in the editor.
     <body>				
         <table border="0" cellpadding="0" cellspacing="0"  align="center" style="max-width:600;background-color:#ffffc1;text-align:center;">
             <tr Style="background-color:#e12258;width:100%;">
-                <td colspan="2"><img src="<?=URL::base(TRUE, FALSE)?>assets/img/logo_gift.png" style="text-align:center;"></td>
+                <td colspan="2"><img src="'.URL::base(TRUE, FALSE).'assets/img/logo_gift.png" style="text-align:center;"></td>
             </tr>
             <tr Style="background-color:#e12258;width:100%;">
                 <td colspan="2"><h2 style="text-align:center;font-family:\'GilgongoKaps-Regular\';font-size:50px; color:#ffffc1;letter-spacing:8px; margin:0;padding:0;">Ajándék<span style="font-family:\'GilgongoMutombo-Regular\';font-size:35px;">kupon</span></h2></td>
             </tr>
             <tr><td colspan="2" style="height: 10px;"></td></tr>
             <tr>
-                <td colspan="2" background="<?=URL::base(TRUE, FALSE)?>assets/img/design_bg.png" height:47px;">
+                <td colspan="2" background="'.URL::base(TRUE, FALSE).'assets/img/design_bg.png" height:47px;">
                 </td>
             </tr>
             <tr style="width:100%;" class="box-row">
@@ -812,14 +812,14 @@ and open the template in the editor.
                 <td style="width:50%;border-left:6px solid #ffffc1;border-right:6px solid #ffffc1;border-bottom:6px solid #ffffc1;text-align:left;padding:15px;padding-bottom: 0px; background-color:#e12258;font-family: \'gillsansmt\'; color:#ffffc1;">' . $final . '</td>
             </tr>
             <tr style=" width:100%;">
-                <td  colspan="2" style=" text-align: center; padding-top:8px;"><img src="<?=URL::base(TRUE, FALSE)?>assets/img/main_bg.png" width="336" height="123"></td>
+                <td  colspan="2" style=" text-align: center; padding-top:8px;"><img src="'.URL::base(TRUE, FALSE).'assets/img/main_bg.png" width="336" height="123"></td>
             </tr>
         </table>
     </body>
 </html>';
 
                         $pdf = new DOMPDF();
-                        $pdf->load_html($file, 'UTF-8');
+                        $pdf->load_html($file);
                         $pdf->render();
                         $output = $pdf->output();
                         file_put_contents(DOCROOT . 'orders/gift_' . $ord->id . '.pdf', $output);
