1a78298c5e78efbf91d4b1c3594983ca91b873fa
Alex Karamushko <alexander.karamushko@gmail.com>
2014-10-19 20:28:28 +0300

    fixes

--- application/views/user_session/login.php
+++ application/views/user_session/login.php
@@ -3,12 +3,13 @@
     <div class="claim-form-container">
         <div class="claim-form">
             <form action="<?php echo URL::site('user_session/login'); ?>" method="POST" class="login">
-                <div class="add">
+                <div class="add" style="margin-bottom: 5px">
                     <input class="rounded" type="text" name="username" placeholder="Email address">
                 </div>
-                <div class="add">
+                <div class="add" style="margin-bottom: 5px">
                     <input class="rounded" type="password" name="password" placeholder="Password">
                 </div>
+                <div class="clear"></div>
                 <div class="clear"></div>
                 <div class="claim-form-btn"><button class="claim-btn rounded" type="submit">Sign in</button></div>
             </form>
