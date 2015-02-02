b5df30af89dfeffa898afccb1ec2e4b0bf0c7fdd
Alex <alexander.karamushko@gmail.com>
2015-02-02 20:37:22 +0200

    fixes

--- application/views/gyik/index.php
+++ application/views/gyik/index.php
@@ -15,15 +15,23 @@
         <h3>2. Hogyan tudok rendelni a kutyusomnak?</h3>
         <div>
             <div class="content">
-                Saját kutyusnak a "Megrendelem" gomb megnyomásával indul a vásárlás folyamata. 
-				<br /><br />
-				Először a kutyusod méretét kell megadnod és néhány fontos információt kérünk róla. Hogy gondosan tudjunk pakolni, nagyon fontos, hogy helyesen töltsd ki az adatokat, különös figyelmet fordítva az allergia kérdésre, hiszen ebben az esetben külön figyelmet kell fordítanunk a megfelelő táp vagy snack beszerzésére. 
-				<br /><br />
-				A második lépésben a periódust tudod kiválasztani, amivel jelzed nekünk, melyik dobozt kéred (Goodiebox Smart vagy Goodiebox Plus), és szeretnéd-e előrendelni. 
-				<br /><br />
-				A harmadik lépésben pedig a személyes adataidat kell megadnod. Mindig ellenőrizd a kitöltött adatokat, hogy azok helyesek legyenek és megfeleljenek a valóságnak. Akkor sikeres a megrendelés, amikor a PayU biztonságos fizető felületén befejezted a szükséges tranzakciót. Ezután email üzeneteket küldünk neked aszerint, hogy milyen jellegű megrendelést tettél. 
-				<br /><br />
-				A megrendelés egyben regisztráció is, létrejön a saját profilod a weboldalunkon, ahova bármikor visszalátogathatsz, nyomon tudod követni az aktuális rendelésedet vagy szükség esetén módosíthatod az adataidat, kivéve az email címedet.
+                Saját kutyusnak a "Megrendelem" gomb megnyomásával indul a vásárlás folyamata. <br/><br/>
+
+Először a kutyusod méretét kell megadnod és néhány fontos információt kérünk róla. Hogy gondosan tudjunk pakolni, nagyon fontos, hogy helyesen töltsd ki az adatokat, különös figyelmet fordítva az allergia kérdésre, hiszen ebben az esetben külön figyelmet kell fordítanunk a megfelelő táp vagy snack beszerzésére. <br/><br/>
+
+A második lépésben a periódust tudod kiválasztani, amivel jelzed nekünk, melyik dobozt kéred (Goodiebox Smart vagy Goodiebox Plus), és szeretnéd-e előrendelni. <br/><br/>
+
+A harmadik lépésben pedig a személyes adataidat kell megadnod. Mindig ellenőrizd a kitöltött adatokat, hogy azok helyesek legyenek és megfeleljenek a valóságnak. Akkor sikeres a megrendelés, amikor kiválasztottad a fizetési  lehetőségek közül a neked szimpatikusabbat és elfogadva az ÁSZF-ben leírtakat a Megrendelem gombra kattintasz.<br/><br/>
+
+Azonnali bankkártyás fizetés esetén automatikusan a PayU biztonságos felületre navigálunk és a bankkáryta adataid kitöltésével rendelhetsz. Mint szolgáltató, mi semmilyen a bankkártyádra vonatkozó adatot nem tárolunk. Ilyen esetben többlet költség nem kerül felszámításra. <br/><br/>
+
+Utánvételes fizetés esetén a Megrendelem gombra kattintva elfogadod, hogy a csomag átvételekor fogsz fizetni készpénzben. Ebben az esetben többlet költséget számolunk fel, melynek összegét minden esetben jelezzük neked, amikor kiválasztod ezt a fizetési lehetőséget. <br/><br/>
+
+A megrendelés egyben regisztráció is, létrejön a saját profilod a weboldalunkon, ahova bármikor visszalátogathatsz, nyomon tudod követni az aktuális rendelésedet vagy szükség esetén később módosíthatod az adataidat, kivéve az email címedet.<br/><br/>
+
+A megrendelést követően két email üzenetet kell kapnod. Az egyik a sikeres regisztrációról szól, míg a második a megrendelésed visszaigazolása, mely tartalmazza az előszámlát és azt is, hogy mikor szállítjuk a megrendelésed.<br/><br/>
+
+FONTOS! Amennyiben nem kapod meg az üzeneteket 24 órán belül, mindenképpen jelezd felénk a <a href="mailto:rendeles@goodiebox.hu">rendeles@goodiebox.hu</a> email címen! Köszönjük!
             </div>
         </div>
 
@@ -37,18 +45,21 @@
         <h3>4. Mennyibe kerül és hogyan fizethetek?</h3>
         <div>
             <div class="content">
-                Rendeléskor a periódusokhoz rendeltük az aktuális árakat. Jelenleg kétféle dobozunk elérhető:
-				<br /><br />
-				Goodiebox Smart 4890 Ft<br />
-				Havonta változó, 3-4 különböző, gondosan válogatott termék (táp, snack, játék, kiegészítő, vitamin) egy dobozban.
-				<br /><br />
-				Goodiebox Plus 7990 Ft<br /> 
-				Havonta változó, 4-5 különböző, gondosan válogatott, prémium minőségi termék (táp, snack, játék, kiegészítő, vitamin) egy dobozban.
-				<br /><br />
-				Ez az ár fix, nincs rejtett költség, hiszen mind a csomagolás, mind pedig a házhozszállítás ingyenes. 
-				Ha van kedvezmény kuponod, természetesen azt a rendelés során fel tudod használni. 
-				<br /><br />
-				Minden esetben csak és kizárólag bankkártyás fizetésre van lehetőség. Amennyiben minden adatot helyesen adtál meg és elfogadtad az ÁSZF-ben leírtakat, tovább léphetsz a PayU biztonságos fizető felületünkre.
+                Rendeléskor a periódusokhoz rendeltük az aktuális árakat. Jelenleg kétféle dobozunk elérhető: <br/><br/>
+
+Goodiebox Smart 4890 Ft<br/>
+Havonta változó, 3-4 különböző, gondosan válogatott termék (táp, snack, játék, kiegészítő, vitamin) egy dobozban. <br/><br/>
+
+Goodiebox Plus 7990 Ft<br/>
+Havonta változó, 4-5 különböző, gondosan válogatott, prémium minőségi termék (táp, snack, játék, kiegészítő, vitamin) egy dobozban. <br/><br/>
+
+Ez az ár fix, nincs rejtett költség, hiszen mind a csomagolás, mind pedig a házhozszállítás ingyenes (bizonyos esetben). Ha van kedvezmény kuponod, természetesen azt a rendelés során fel tudod használni. <br/><br/>
+
+Fizetési lehetőségek:<br/><br/>
+
+Azonnali bankkártyás fizetés (PayU biztonságos felületen keresztül, amikor többlet kötlség nem kerül felszámolásra!)<br/><br/>
+
+Utánvételes fizetés (Átvétekor a futárnak tudsz fizetni készpénzzel. Ebben az esetben többlet költség kerül felszámolásra, melyet a megrendeléskor jelezni is fogunk neked!)
             </div>
         </div>
 
@@ -81,14 +92,23 @@ A megrendelt Goodiebox-ot a rendelési időszak lezárását követően, körül
         <h3>8. Van bármilyen rejtett költség?</h3>
         <div>
             <div class="content">
-                A doboz ára adott, azon kívül semmilyen rejtett költség nincsen. A házhozszállítás ingyenes.
+                A doboz ára adott, azon kívül semmilyen rejtett költség nincsen. A házhozszállítás ingyenes, amennyiben a bankkártyás fizetési lehetőséget választod. Utánvételes fizetés esetén többlet költséget számolunk fel, melyet a fizetési lehetőség kiválasztásakor jelzünk is neked!
             </div>
         </div>
 
         <h3>9. Nem érkezett meg a Goodiebox, pedig regsztráltam, vagyis megrendeltem.</h3>
         <div>
             <div class="content">
-                Amennyiben nem érkezett meg a teljesítésről szóló e-mail a rendelést követően azonnal, mindenképpen jelezd azt ügyfélszolgálati e-mail címeink egyikén, hogy utána tudjunk nézni: rendeles@goodiebox.hu vagy info@goodiebox.hu.
+                Amennyiben nem érkezett meg a megrendelést visszaigazoló e-mail a rendelést követően azonnal vagy legfeljebb 24 órán belül, mindenképpen jelezd azt ügyfélszolgálati e-mail címünkön, hogy utána tudjunk nézni: rendeles@goodiebox.hu
+                <br /><br />
+                Saját kutyusnak a "Megrendelem" gomb megnyomásával indul a vásárlás folyamata. 
+<br /><br />
+                Először a kutyusod méretét kell megadnod és néhány fontos információt kérünk róla. Hogy gondosan tudjunk pakolni, nagyon fontos, hogy helyesen töltsd ki az adatokat, különös figyelmet fordítva az allergia kérdésre, hiszen ebben az esetben külön figyelmet kell fordítanunk a megfelelő táp vagy snack beszerzésére. 
+<br /><br />
+                A második lépésben a periódust tudod kiválasztani, amivel jelzed nekünk, melyik dobozt kéred (Goodiebox Smart vagy Goodiebox Plus), és szeretnéd-e előrendelni. 
+<br /><br />
+                A harmadik lépésben pedig a személyes adataidat kell megadnod. Mindig ellenőrizd a kitöltött adatokat, hogy azok helyesek legyenek és megfeleljenek a valóságnak. Akkor sikeres a megrendelés, amikor a PayU biztonságos fizető felületén befejezted a szükséges tranzakciót. Ezután email üzeneteket küldünk neked aszerint, hogy milyen jellegű megrendelést tettél. 
+
             </div>
         </div>
 
