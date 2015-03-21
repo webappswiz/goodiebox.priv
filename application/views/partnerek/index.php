e130db4ceca1f2fc24ac7275ed7b6de8240306a6
karam <karam@karam.org.ua>
2015-03-21 12:23:39 +0200

    fixes

--- application/views/partnerek/index.php
+++ application/views/partnerek/index.php
@@ -3,14 +3,14 @@
     <br/>
     <div class="image-row" style="text-align: center;">
         <div class="image-set">
-            <img class="" height="60" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0267_300.jpg" alt=""/></a>
-            <img class="" height="60" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0268_300.jpg" alt="" /></a><br/><br/><br/>
-            <img class="example-image" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0269_300.jpg" alt="" /></a>
-            <img class="" height="150" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0270_300.jpg" alt="" /></a>
-            <img class="example-image" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0271_300.jpg" alt="" /></a>
-            <img class="example-image" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0272_300.jpg" alt="" /></a>
-            <img class="example-image" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0304_300.jpg" alt="" /></a>
-            <img class="example-image" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0305_300.jpg" alt="" /></a>
+            <a href="https://www.purina-proplan.hu/" target="blank"><img class="" height="60" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0267_300.jpg" alt=""/></a>
+            <a href="http://www.alpha-vet.hu/" target="blank"><img class="" height="60" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0268_300.jpg" alt="" /></a><br/><br/><br/>
+            <a href="http://bol-dog.hu/" target="blank"><img class="example-image" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0269_300.jpg" alt="" /></a>
+            <a href="http://spicedogs.eu/hu" target="blank"><img class="" height="150" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0270_300.jpg" alt="" /></a>
+            <a href="http://kutyabarat.hu/" target="blank"><img class="example-image" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0271_300.jpg" alt="" /></a>
+            <a href="http://www.flashkutyapekseg.hu/" target="blank"><img class="example-image" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0272_300.jpg" alt="" /></a>
+            <a href="http://sixfeet-design.blogspot.hu/" target="blank"><img class="example-image" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0304_300.jpg" alt="" /></a>
+            <a href="http://beagle-club.hu/" target="blank"><img class="example-image" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0305_300.jpg" alt="" /></a>
         </div>
     </div>
 </section>
