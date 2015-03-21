4b84f5e86ed7fd8f19957e051c14a3ab92ad9b6d
karam <karam@karam.org.ua>
2015-03-21 12:17:10 +0200

    fixes

--- application/views/partnerek/index.php
+++ application/views/partnerek/index.php
@@ -3,18 +3,12 @@
     <br/>
     <div class="image-row" style="text-align: center;">
         <div class="image-set">
-            <a class="example-image-link" href="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0267.jpg" data-lightbox="example-set1" data-title="Bezáráshoz kattints bárhova a fotón kívülre vagy az x gombra!
-               "><img class="" height="60" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0267_300.jpg" alt=""/></a>
-            <a class="example-image-link" href="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0268.jpg" data-lightbox="example-set1" data-title="Bezáráshoz kattints bárhova a fotón kívülre vagy az x gombra!
-               "><img class="" height="60" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0268_300.jpg" alt="" /></a><br/><br/><br/>
-            <a class="example-image-link" href="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0269.jpg" data-lightbox="example-set1" data-title="Bezáráshoz kattints bárhova a fotón kívülre vagy az x gombra!
-               "><img class="example-image" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0269_300.jpg" alt="" /></a>
-            <a class="example-image-link" href="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0270.jpg" data-lightbox="example-set1" data-title="Bezáráshoz kattints bárhova a fotón kívülre vagy az x gombra!
-               "><img class="" height="150" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0270_300.jpg" alt="" /></a>
-            <a class="example-image-link" href="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0271.jpg" data-lightbox="example-set1" data-title="Bezáráshoz kattints bárhova a fotón kívülre vagy az x gombra!
-               "><img class="example-image" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0271_300.jpg" alt="" /></a>
-            <a class="example-image-link" href="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0272.jpg" data-lightbox="example-set1" data-title="Bezáráshoz kattints bárhova a fotón kívülre vagy az x gombra!
-               "><img class="example-image" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0272_300.jpg" alt="" /></a>
+            <img class="" height="60" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0267_300.jpg" alt=""/></a>
+            <img class="" height="60" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0268_300.jpg" alt="" /></a><br/><br/><br/>
+            <img class="example-image" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0269_300.jpg" alt="" /></a>
+            <img class="" height="150" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0270_300.jpg" alt="" /></a>
+            <img class="example-image" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0271_300.jpg" alt="" /></a>
+            <img class="example-image" src="<?= URL::base(TRUE, FALSE) ?>/assets/img/partners/img_0272_300.jpg" alt="" /></a>
         </div>
     </div>
 </section>
