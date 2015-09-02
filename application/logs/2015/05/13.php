6f75d0f1112ce6fa6ee543ff9bf5d03fe4d1355a
karam <karam@karam.org.ua>
2015-09-02 22:43:31 +0300

    fixes

--- application/logs/2015/05/13.php
+++
@@ -1,5 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.'); ?>
-
-2015-05-13 21:25:00 --- EMERGENCY: ErrorException [ 4 ]: syntax error, unexpected ' ~ APPPATH/views/main/index.php [ 308 ] in file:line
-2015-05-13 21:25:00 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
-#1 {main} in file:line
