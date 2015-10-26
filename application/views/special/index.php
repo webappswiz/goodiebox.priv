c71eb6592de6ac50e581b5c4ffd6c219613a6a72
Alex Karamushko <you@example.com>
2015-10-26 21:30:33 +0200

    fixes

--- application/views/special/index.php
+++ application/views/special/index.php
@@ -8,81 +8,84 @@
     <p style="padding-top: 5px;font-family: 'franklin_gothic_bookregular'; font-size: 18px; line-height: 30px;"><?php echo __('Minden megrendelt Goodiebox tartalmaz egy direkt erre a célra gyártott kedvezmény kártyát. Ha kinézted a listából, hogy melyik szolgáltató ajánlata a legszimpatikusabb az adott hónapban, nincs más dolog, mint kezedbe venni a kedvezmény kártyát, öledbe kapni a kutyusodat és már indulhattok is beváltani! Mivel a kártyát le kell adnod az általad választott szolgáltatónál, így minden megrendelt Goodiebox meglepidobozzal csak egy szolgáltatás vehető kedvezményesen igénybe egy adott hónapban. De ne aggódjatok, mindig lesz újabb hónap, újabb Goodiebox és újabb speciális ajánlatokra beváltható kedvezmény kártya is! :)'); ?></p>
     <center><img style="margin: 0 auto;width:600px" src="<?= URL::base(TRUE, FALSE) ?>assets/img/ok-main-page-pink.jpg"></center>
     <div id="collapse-content">
-        <h2><?php echo __('NEKED') ?></h2>
-        <br/>
-        <h3><?php echo __('1. Kávézók'); ?></h3>
+        <h3><?php echo __('NEKED') ?></h3>
         <div>
             <div class="content" style="text-align:justify;">
+                <h3 class="collapse_content"><?php echo __('1. Kávézók'); ?></h3>
+                <div class="cnt">
+                    <div class="content" style="text-align:justify;">
+                        ssss
+                    </div>
+                </div>
+                <h3 class="collapse_content"><?php echo __('2. Éttermek'); ?></h3>
+                <div class="cnt">
+                    <div class="content" style="text-align:justify;">
 
+                    </div>
+                </div>
+                <h3 class="collapse_content"><?php echo __('3. Sütizők'); ?></h3>
+                <div class="cnt">
+                    <div class="content" style="text-align:justify;">
+
+                    </div>
+                </div>
+                <h3 class="collapse_content"><?php echo __('4. Boltok'); ?></h3>
+                <div class="cnt">
+                    <div class="content" style="text-align:justify;">
+
+                    </div>
+                </div>
             </div>
         </div>
-        <h3><?php echo __('2. Éttermek'); ?></h3>
-        <div>
+        <h3><?php echo __('NEKI'); ?></h3>
+        <div class="main-collapse">
             <div class="content" style="text-align:justify;">
+                <div class="collapse_content1">
+                    <h3 class="collapse_content"><?php echo __('1. Sütizők'); ?></h3>
+                    <div class="cnt">
+                        <div class="content" style="text-align:justify;">
+                            <table>
+                                <tr>
+                                    <td>Flash kutyapékség<br/>
+                                        1094 Budapest Páva utca 17.<br/>
+                                        +36 70 328 0870<br/>
+                                        info@flashkutyapekseg.hu
+                                    </td>
+                                    <td>
+                                        <img style="margin: 0px 0px 0px 45px;" width="72" height="72" src="<?= URL::base(TRUE, FALSE) ?>assets/img/5-icon.jpg">
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <h3 class="collapse_content"><?php echo __('2. Kozmetikák'); ?></h3>
+                    <div class="cnt">
+                        <div class="content" style="text-align:justify;">
 
-            </div>
-        </div>
-        <h3><?php echo __('3. Sütizők'); ?></h3>
-        <div>
-            <div class="content" style="text-align:justify;">
+                        </div>
+                    </div>
+                    <h3 class="collapse_content"><?php echo __('3. Doktorbácsik/nénik'); ?></h3>
+                    <div class="cnt">
+                        <div class="content" style="text-align:justify;">
 
-            </div>
-        </div>
-        <h3><?php echo __('4. Boltok'); ?></h3>
-        <div>
-            <div class="content" style="text-align:justify;">
+                        </div>
+                    </div>
+                    <h3 class="collapse_content"><?php echo __('4. Panziók'); ?></h3>
+                    <div class="cnt">
+                        <div class="content" style="text-align:justify;">
 
-            </div>
-        </div>
+                        </div>
+                    </div>
+                    <h3 class="collapse_content"><?php echo __('4. Egyéb szolgáltatások'); ?></h3>
+                    <div class="cnt">
+                        <div class="content" style="text-align:justify;">
 
-
-        <h2><?php echo __('NEKI'); ?></h2>
-        <br/>
-        <h3><?php echo __('1. Sütizők'); ?></h3>
-        <div>
-            <div class="content" style="text-align:justify;">
-                <table>
-                    <tr>
-                        <td>Flash kutyapékség<br/>
-                            1094 Budapest Páva utca 17.<br/>
-                            +36 70 328 0870<br/>
-                            info@flashkutyapekseg.hu
-                        </td>
-                        <td>
-                            <img style="margin: 0px 0px 0px 45px;" width="72" height="72" src="<?= URL::base(TRUE, FALSE) ?>assets/img/5-icon.jpg">
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-        <h3><?php echo __('2. Kozmetikák'); ?></h3>
-        <div>
-            <div class="content" style="text-align:justify;">
-
-            </div>
-        </div>
-        <h3><?php echo __('3. Doktorbácsik/nénik'); ?></h3>
-        <div>
-            <div class="content" style="text-align:justify;">
-
-            </div>
-        </div>
-        <h3><?php echo __('4. Panziók'); ?></h3>
-        <div>
-            <div class="content" style="text-align:justify;">
-
-            </div>
-        </div>
-        <h3><?php echo __('4. Egyéb szolgáltatások'); ?></h3>
-        <div>
-            <div class="content" style="text-align:justify;">
-
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-
-
-
 
 
 
@@ -97,6 +100,16 @@
                 this.css({height: "0px"});
                 this.removeClass("open");
             }
+        });
+        $('.collapse_content').on('click', function () {
+            new_height = $('.main-collapse.open').outerHeight() - $('.cnt.open').outerHeight();
+            $('.main-collapse.open').outerHeight(new_height);
+            $('.cnt').css({height: "0px"});
+            $('.cnt').removeClass('open');
+            $(this).next().addClass("open");
+            $(this).next().outerHeight($(this).next().children().outerHeight());
+            height = $('.main-collapse.open').outerHeight() + $(this).next().children().outerHeight();
+            $('.main-collapse.open').outerHeight(height);
         });
     </script>
 </section>
