eccd381369edc391c27e9564db8006c199ab2286
Goodiebox <info@goodiebox>
2015-10-27 12:25:32 -0600

    database.php

--- forum/cache/sql_b152bea2b86cc301f01da4d7125e73ba.php
+++ forum/cache/sql_b152bea2b86cc301f01da4d7125e73ba.php
@@ -1,5 +1,5 @@
 <?php exit; ?>
-1445968823
+1445969469
 SELECT forum_id, forum_name, parent_id, forum_type, left_id, right_id FROM phpbb_forums ORDER BY left_id ASC
 1065
 a:6:{i:0;a:6:{s:8:"forum_id";s:1:"1";s:10:"forum_name";s:26:"Placc etikett (olvass el!)";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"1";s:8:"right_id";s:1:"2";}i:1;a:6:{s:8:"forum_id";s:1:"3";s:10:"forum_name";s:38:"Hogyan éljünk együtt egy kutyussal?";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"3";s:8:"right_id";s:1:"4";}i:2;a:6:{s:8:"forum_id";s:1:"4";s:10:"forum_name";s:9:"Cserebere";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"5";s:8:"right_id";s:1:"6";}i:3;a:6:{s:8:"forum_id";s:1:"5";s:10:"forum_name";s:21:"Goodiebox? Goodiebox!";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"7";s:8:"right_id";s:1:"8";}i:4;a:6:{s:8:"forum_id";s:1:"7";s:10:"forum_name";s:15:"Unboxing videó";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"9";s:8:"right_id";s:2:"10";}i:5;a:6:{s:8:"forum_id";s:1:"6";s:10:"forum_name";s:24:"Kutyás szolgáltatások";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:2:"11";s:8:"right_id";s:2:"12";}}
