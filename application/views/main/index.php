8f698b57d2ec4b23e80fa75e8e01189cfa8cb998
webappswiz <webappswiz@users.noreply.github.com>
2015-11-02 17:29:30 +0100

    Update index.php

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -144,10 +144,10 @@ $menus = array(
     <div class="count-text">
 
         <p><?= $text ?></p>
-        <div class="lock">
+        <div class="counter">
             <p><?php echo __('Hátralévő idő:'); ?> <span id="days" class="count-btn rounded">12</span> <?php echo __('nap'); ?> <span id="hours" style="margin-right: 10px;">15:37:55</span></p>
         </div>
-        <div class="counter">
+        <div class="lock">
             <ul>
                 <li><a href="#"><?php echo __('Nyitva vagyunk!'); ?></a></li>
             </ul>
