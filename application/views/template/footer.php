fd6736f0682a3c35770f8c76c370b555cf61b773
webappswiz <github@webappswiz.com>
2015-12-01 01:25:13 +0100

    snow tweaks

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -22,9 +22,10 @@
 <script src="<?= URL::base(TRUE, FALSE) ?>assets/js/snowstorm.js"></script>
 		<script>
 		snowStorm.snowColor = '#99ccff';   // blue-ish snow!?
-		snowStorm.flakesMaxActive = 36;    // show more snow on screen at once
+		snowStorm.flakesMaxActive = 66;    // show more snow on screen at once
 		snowStorm.useTwinkleEffect = true; // let the snow flicker in and out of view
 		snowStorm.excludeMobile = false;
+		snowStorm.animationInterval = 50;
 		</script>
 </body>
 </html>
