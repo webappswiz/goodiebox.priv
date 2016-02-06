cca0af4e02675ec9233f49fde42f0bc2740dc78a
webappswiz <github@webappswiz.com>
2016-02-06 23:33:37 +0100

    disable timer

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -150,7 +150,7 @@ $menus = array(
     <div class="count-text">
 
         <p><?= $text ?></p>
-        <div class="counter" style="dispaly:none;">
+        <div class="counter" style="display:none;">
             <p><?php echo __('Hátralévő idő:'); ?> <span id="days" class="count-btn rounded">12</span> <?php echo __('nap'); ?> <span id="hours" style="margin-right: 10px;">15:37:55</span></p>
         </div>
         <div class="lock">
